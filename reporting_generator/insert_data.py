78a37002d48827aca8c6d844e7fbfbc4b4f48161
surybang <hos.fabien@outlook.com>
2025-06-01 21:50:51 +0000

    style: docstrings typos

--- reporting_generator/insert_data.py
+++ reporting_generator/insert_data.py
@@ -3,12 +3,12 @@ import pandas as pd
 
 def write_data_to_excel(path_file: str, df: pd.DataFrame, sheet_name: str = 'DATA') -> None:
     """
-    Écrit un DataFrame dans une feuille Excel (ajout au fichier existant).
+    Write a DataFrame in an excel sheet (create or replace).
 
     Args:
-        path_file (str): Chemin du fichier Excel à modifier.
-        df (pd.DataFrame): Données à écrire.
-        sheet_name (str): Nom de la feuille à créer ou remplacer.
+        path_file (str): Path to the file.
+        df (pd.DataFrame): Data.
+        sheet_name (str): Name of the sheet to create or replace.
     """
     with pd.ExcelWriter(
         path_file,
