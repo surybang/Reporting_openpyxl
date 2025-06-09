506eabf79bf8b4df461da80287d131d52c01a9a9
surybang <hos.fabien@outlook.com>
2025-06-09 14:05:33 +0000

    feat: import ReportingJob

--- reporting_generator/__init__.py
+++ reporting_generator/__init__.py
@@ -1,4 +1,5 @@
 from .fill_indicators import fill_indicators
 from .insert_data import write_data_to_excel
+from .reporting_job import ReportingJob
 
-__all__ = ['fill_indicators', 'write_data_to_excel']
+__all__ = ['fill_indicators', 'write_data_to_excel', 'ReportingJob']
